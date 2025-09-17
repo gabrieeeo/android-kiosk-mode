30cd822f61204104604c50f049dcccbd58f59636
gabriellima <gabrieeelima15@gmail.com>
2025-09-17 16:01:57 -0300

    atualizando o projeto v0.1

--- app/src/main/java/me/gabrieeeo/kiosk/MainActivity.java
+++ app/src/main/java/me/gabrieeeo/kiosk/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.UserManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
